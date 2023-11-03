755d3f1e90d7ceb98616f293e7a8dc705762bad0
MaryAngeleen16 <angelpagalan38@gmail.com>
2023-11-03 23:03:43 +0800

    footer done

--- frontend/src/Components/Layouts/Footer.js
+++ frontend/src/Components/Layouts/Footer.js
@@ -1,28 +1,20 @@
 import React from 'react';
 import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
-
+import './FH.css';
 export default function App() {
   return (
-    <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
+    <MDBFooter className='text-center text-lg-start text-muted JsonFooter'>
       {/* <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
         <div className='me-5 d-none d-lg-block'>
         </div>
       </section> */}
 
-      <section className=''>
+      <section className='JsonFooter'>
         <MDBContainer className='text-center text-md-start mt-5'>
           <MDBRow className='mt-3'>
-            <MDBCol md="3" lg="4" xl="3" className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>
-                <MDBIcon icon="gem" className="me-3" />
-                JSONBrews
-              </h6>
-              <p>
-                Mga Bading 
-              </p>
-            </MDBCol>
 
-            <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
+
+            <MDBCol md="4" lg="4" xl="4" className='mx-auto mb-4'>
               <h6 className='text-uppercase fw-bold mb-4'>Creators</h6>
               <p>Baluyot, Kayla Mae B.</p>
               <p>Elumba, Bea Clarisse B.</p>
@@ -30,7 +22,7 @@ export default function App() {
               <p>Pagalan, Mary Angeleen</p>
             </MDBCol>
 
-            <MDBCol md="4" lg="3" xl="3" className='mx-auto mb-md-0 mb-4'>
+            <MDBCol md="4" lg="4" xl="4" className='mx-auto mb-4'>
               <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
               <p>
                 <MDBIcon icon="home" className="me-2" />
@@ -38,7 +30,7 @@ export default function App() {
               </p>
               <p>
                 <MDBIcon icon="envelope" className="me-3" />
-                jsonbrews@gnail.com
+                jsonbrews@gmail.com
               </p>
               <p>
                 <MDBIcon icon="phone" className="me-3" /> + 63 888 999 40
@@ -51,12 +43,12 @@ export default function App() {
         </MDBContainer>
       </section>
 
-      <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
-        © 2021 Copyright:
-        <a className='text-reset fw-bold' href=' '>
+      <div className='text-center p-4 JsonFooter' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+        ©2023
+        <a style={{ paddingLeft: '20px' }} className='text-reset' href=' '>
           JSONBrews.com
         </a>
       </div>
     </MDBFooter>
   );
-}
+};

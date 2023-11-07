de4aaecf612225f67404d619d1f82d4a3de7026e
baluyotkaylamae <kaylamae.baluyot@tup.edu.ph>
2023-11-07 15:35:02 +0800

    modify Details

--- frontend/src/Components/Product/ProductDetails.js
+++ frontend/src/Components/Product/ProductDetails.js
@@ -66,19 +66,6 @@ const ProductDetails = ({ addItemToCart, cartItems }) => {
             <div className="col-12 col-lg-5 mt-5">
               <h3>{product.name}</h3>
               <p id="product_id">Product # {product._id}</p>
-
-              <hr />
-
-              <div className="rating-outer">
-                <div
-                  className="rating-inner"
-                  style={{ width: `${(product.ratings / 5) * 100}%` }}
-                ></div>
-              </div>
-              <span id="no_of_reviews">({product.numOfReviews} Reviews)</span>
-
-              <hr />
-
               <p id="product_price">${product.price}</p>
               <div className="stockCounter d-inline">
                 <span className="btn btn-danger minus" onClick={decreaseQty}>
@@ -94,16 +81,18 @@ const ProductDetails = ({ addItemToCart, cartItems }) => {
                   +
                 </span>
               </div>
-              <button
-                type="button"
-                id="cart_btn"
-                className="btn btn-primary d-inline ml-4"
-                disabled={product.stock === 0}
-                onClick={addToCart}
-              >
-                Add to Cart
-              </button>
 
+              <div>
+                <button
+                  type="button"
+                  id="cart_btn"
+                  className="btn btn-primary d-inline ml-4"
+                  disabled={product.stock === 0}
+                  onClick={addToCart}
+                >
+                  Add to Cart
+                </button>
+              </div>
               <hr />
 
               <p>
@@ -122,78 +111,6 @@ const ProductDetails = ({ addItemToCart, cartItems }) => {
               <p>{product.description}</p>
 
               <hr />
-
-              <p id="product_seller mb-3">
-                Sold by: <strong>{product.seller}</strong>
-              </p>
-
-              <button
-                id="review_btn"
-                type="button"
-                className="btn btn-primary mt-4"
-                data-toggle="modal"
-                data-target="#ratingModal"
-              >
-                Submit Your Review
-              </button>
-
-              <div
-                className="modal fade"
-                id="ratingModal"
-                tabIndex="-1"
-                role="dialog"
-                aria-labelledby="ratingModalLabel"
-                aria-hidden="true"
-              >
-                <div className="modal-dialog" role="document">
-                  <div className="modal-content">
-                    <div className="modal-header">
-                      <h5 className="modal-title" id="ratingModalLabel">
-                        Submit Review
-                      </h5>
-                      <button
-                        type="button"
-                        className="close"
-                        data-dismiss="modal"
-                        aria-label="Close"
-                      >
-                        <span aria-hidden="true">&times;</span>
-                      </button>
-                    </div>
-                    <div className="modal-body">
-                      <ul className="stars">
-                        <li className="star">
-                          <i className="fa fa-star"></i>
-                        </li>
-                        <li className="star">
-                          <i className="fa fa-star"></i>
-                        </li>
-                        <li className="star">
-                          <i className="fa fa-star"></i>
-                        </li>
-                        <li className="star">
-                          <i className="fa fa-star"></i>
-                        </li>
-                        <li className="star">
-                          <i className="fa fa-star"></i>
-                        </li>
-                      </ul>
-                      <textarea
-                        name="review"
-                        id="review"
-                        className="form-control mt-3"
-                      ></textarea>
-                      <button
-                        className="btn my-3 float-right review-btn px-4 text-white"
-                        data-dismiss="modal"
-                        aria-label="Close"
-                      >
-                        Submit
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
             </div>
           </div>
         </Fragment>

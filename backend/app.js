f6bb05bab0f94043999c227e2845bd71d57748ed
baluyotkaylamae <kaylamae.baluyot@tup.edu.ph>
2023-11-08 06:47:41 +0800

    cart and transaction

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const categories = require('./routes/category');
 const products = require('./routes/product');
+
 const auth = require('./routes/auth');
 
 const app = express();
@@ -16,5 +17,6 @@ app.use(cookieParser());
 app.use('/api', categories);
 app.use('/api', products);
 app.use('/api', auth);
+
 
 module.exports = app
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 import Header from './Components/Layouts/Header';
 import Footer from './Components/Layouts/Footer'
 import CreateCategory from './Components/Admin/CreateCategory';
@@ -19,9 +21,88 @@ import NewPassword from './Components/User/NewPassword';
 import UpdatePassword from './Components/User/UpdatePassword';
 import Dashboard from './Components/Admin/Dashboard';
 import Home from './Components/Home';
-import ProductDetails from './Components/Product/ProductDetails'
+import ProductDetails from './Components/Product/ProductDetails';
+import Cart from './Components/Cart/Cart';
+import Shipping from './Components/Cart/Shipping';
+import ConfirmOrder from './Components/Cart/ConfirmOrder';
+import Payment from './Components/Cart/Payment';
+import OrderSuccess from './Components/Cart/OrderSuccess';
+import ListOrders from './Components/Order/ListOrders';
+import OrderDetails from './Components/Order/OrderDetails';
+import { toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
 
 function App() {
+  const [state, setState] = useState({
+    cartItems: localStorage.getItem('cartItems')
+      ? JSON.parse(localStorage.getItem('cartItems'))
+      : [],
+    shippingInfo: localStorage.getItem('shippingInfo')
+      ? JSON.parse(localStorage.getItem('shippingInfo'))
+      : {},
+  })
+  const addItemToCart = async (id, quantity) => {
+    console.log(id, quantity)
+    try {
+      const { data } = await axios.get(`http://localhost:4001/api/v1/product/${id}`)
+      const item = {
+        product: data.product._id,
+        name: data.product.name,
+        price: data.product.price,
+        image: data.product.images[0].url,
+        stock: data.product.stock,
+        quantity: quantity
+      }
+
+      const isItemExist = state.cartItems.find(i => i.product === item.product)
+      console.log(isItemExist, state)
+      // setState({
+      //   ...state,
+      //   cartItems: [...state.cartItems, item]
+      // })
+      if (isItemExist) {
+        setState({
+          ...state,
+          cartItems: state.cartItems.map(i => i.product === isItemExist.product ? item : i)
+        })
+      }
+      else {
+        setState({
+          ...state,
+          cartItems: [...state.cartItems, item]
+        })
+      }
+
+      toast.success('Item Added to Cart', {
+        position: toast.POSITION.BOTTOM_RIGHT
+      })
+
+    } catch (error) {
+      toast.error(error, {
+        position: toast.POSITION.TOP_LEFT
+      });
+      // navigate('/')
+    }
+
+  }
+
+  const removeItemFromCart = async (id) => {
+    setState({
+      ...state,
+      cartItems: state.cartItems.filter(i => i.product !== id)
+    })
+    localStorage.setItem('cartItems', JSON.stringify(state.cartItems))
+  }
+
+  const saveShippingInfo = async (data) => {
+    setState({
+      ...state,
+      shippingInfo: data
+    })
+    localStorage.setItem('shippingInfo', JSON.stringify(data))
+  }
+
   return (
     <div>
       <Router>
@@ -30,7 +111,9 @@ function App() {
 
           {/*for login */}
           <Route path="/" element={<Home />} exact="true" />
-          <Route path="/product/:id" element={<ProductDetails  />} exact="true" />
+          {/* <Route path="/product/:id" element={<ProductDetails  />} exact="true" /> */}
+          <Route path="/product/:id" element={<ProductDetails cartItems={state.cartItems} addItemToCart={addItemToCart} />} exact="true" />
+          
           <Route path="/login" element={<Login />} exact="true" />
           <Route path="/register" element={<Register />} exact="true" />
           <Route path="/me" element={<Profile />} exact="true" />
@@ -39,6 +122,22 @@ function App() {
           <Route path="/password/reset/:token" element={<NewPassword />} exact="true" />
           <Route path="/password/update" element={<UpdatePassword />} />
 
+          {/* cart */}
+          <Route path="/cart" element={<Cart cartItems={state.cartItems} addItemToCart={addItemToCart} removeItemFromCart={removeItemFromCart} />} exact="true" />
+          <Route path="/shipping" element={<Shipping
+            shipping={state.shippingInfo}
+            saveShippingInfo={saveShippingInfo}
+          />}
+          />
+          <Route path="/confirm" element={<ConfirmOrder cartItems={state.cartItems} shippingInfo={state.shippingInfo} />}  />
+          <Route path="/payment" element={<Payment cartItems={state.cartItems} shippingInfo={state.shippingInfo} />}  />
+          <Route path="/success" element={<OrderSuccess />}  />
+
+          {/* orders */}
+          <Route path="/orders/me" element={<ListOrders />}  />
+          <Route path="/order/:id" element={<OrderDetails />}  />
+
+          {/* category */}
           <Route path="/category/create" element={<CreateCategory />} end />
           <Route path="/category/update/:id" element={<UpdateCategory />} end />
           <Route path="/category/list" element={<CategoryList />} end />

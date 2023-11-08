c3dccd3cfd84bc628f5a41b1ac946fdeb7ebb25e
MaryAngeleen16 <angelpagalan38@gmail.com>
2023-11-08 22:07:28 +0800

    dashboard and sidebar modified

--- frontend/src/Components/Admin/Dashboard.js
+++ frontend/src/Components/Admin/Dashboard.js
@@ -2,12 +2,13 @@ import React, { Fragment, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import MetaData from '../Layouts/Metadata';
-import Loader from '../Layouts/Loader'
+import Loader from '../Layouts/Loader';
 import Sidebar from './Sidebar';
 import { getToken } from '../../utils/helpers';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import '../Layouts/FH.css';
 
 const Dashboard = () => {
     const [categories, setCategories] = useState([]);
@@ -52,25 +53,6 @@ const Dashboard = () => {
         }
     };
 
-    // const getProducts = async () => {
-    //     try {
-    //         const config = {
-    //             headers: {
-    //                 'Content-Type': 'multipart/form-data',
-    //                 'Authorization': `Bearer ${getToken()}`
-    //             }
-    //         };
-
-    //         const { data } = await axios.get(`http://localhost:4001/api/product`, config);
-    //         setUsers(data.products);
-    //         console.log(data.products);
-    //     } catch (error) {
-    //         console.error(error);
-    //         // Handle the error as needed
-    //     }
-    // };
-
-
     useEffect(() => {
         getCategories();
         getUsers();
@@ -79,53 +61,54 @@ const Dashboard = () => {
     return (
         <Fragment>
             <div className="row">
-                <div className="col-12 col-md-2">
+                <div className="col-12 col-md-2 sidebar-dashboard" style={{ padding: 0 }}>
                     <Sidebar />
                 </div>
 
                 <div className="col-12 col-md-10">
-                    <h1 className="my-4">Dashboard</h1>
+                    <div className="container"> {/* Add a new container */}
+                        <h1 className="my-4 dashboard-title">Dashboard</h1>
 
-                    {loading ? (
-                        // Display a loading indicator here if needed
-                        <p>Loading...</p>
-                    ) : (
-                        <Fragment>
-                            <MetaData title={'Admin Dashboard'} />
+                        {loading ? (
+                            // Display a loading indicator here if needed
+                            <p>Loading...</p>
+                        ) : (
+                            <Fragment>
+                                <MetaData title={'Admin Dashboard'} />
 
-                            <div className="row pr-4">
-                                <div className="col-xl-3 col-sm-6 mb-3">
-                                    <div className="card text-white bg-success o-hidden h-100">
-                                        <div className="card-body">
-                                            <div className="text-center card-font-size">Categories<br /> <b>{categories.length}</b></div>
+                                <div className="row pr-4">
+                                    <div className="col-xl-3 col-sm-6 mb-3 ">
+                                        <div className="card text-white bg-success o-hidden h-100 dashboard-category">
+                                            <div className="card-body">
+                                                <div className="text-center card-font-size">Categories<br /> <b>{categories.length}</b></div>
+                                            </div>
+
+                                            <Link className="card-footer text-white clearfix small z-1" to="/admin/categories">
+                                                <span className="float-left">View Details</span>
+                                                <span className="float-right">
+                                                    <i className="fa fa-angle-right"></i>
+                                                </span>
+                                            </Link>
                                         </div>
+                                    </div>
+                                    <div className="col-xl-3 col-sm-6 mb-3">
+                                        <div className="card text-white bg-info o-hidden h-100 dashboard-product">
+                                            <div className="card-body">
+                                                <div className="text-center card-font-size">Users<br /> <b>{users.length}</b></div>
+                                            </div>
 
-                                        <Link className="card-footer text-white clearfix small z-1" to="/admin/categories">
-                                            <span className="float-left">View Details</span>
-                                            <span className="float-right">
-                                                <i className="fa fa-angle-right"></i>
-                                            </span>
-                                        </Link>
+                                            <Link className="card-footer text-white clearfix small z-1" to="/admin/users">
+                                                <span className="float-left">View Details</span>
+                                                <span className="float-right">
+                                                    <i className="fa fa-angle-right"></i>
+                                                </span>
+                                            </Link>
+                                        </div>
                                     </div>
                                 </div>
-                                <div className="col-xl-3 col-sm-6 mb-3">
-                                    <div className="card text-white bg-info o-hidden h-100">
-                                        <div className="card-body">
-                                            <div className="text-center card-font-size">Users<br /> <b>{users.length}</b></div>
-                                        </div>
-
-                                        <Link className="card-footer text-white clearfix small z-1" to="/admin/users">
-                                            <span className="float-left">View Details</span>
-                                            <span className="float-right">
-                                                <i className="fa fa-angle-right"></i>
-                                            </span>
-                                        </Link>
-
-                                    </div>
-                                </div>
-                            </div>
-                        </Fragment>
-                    )}
+                            </Fragment>
+                        )}
+                    </div>
                 </div>
             </div>
         </Fragment>
--- frontend/src/Components/Admin/Sidebar.js
+++ frontend/src/Components/Admin/Sidebar.js
@@ -13,24 +13,25 @@ const Sidebar = () => {
   };
 
   return (
-    <div className="container">
-      <div className="bg-light border-right" id="sidebar">
-        <div className="list-group list-group-flush">
-          <button onClick={toggleCategory} className="list-group-item list-group-item-action" data-toggle="collapse" data-target="#categoryDropdown">
+    <div className="container sidebar-JSON ">
+      <div className="bg-light border-right " id="sidebar">
+        <div className="list-group list-group-flush ">
+          <button onClick={toggleCategory} className="list-group-item list-group-item-action sidebar-dashboard" data-toggle="collapse" data-target="#categoryDropdown">
             CATEGORY
           </button>
           <div id="categoryDropdown" className={`collapse ${isCategoryOpen ? 'show' : ''}`}>
-            <a href="/category/create" className="list-group-item list-group-item-action">Create Category</a>
-            <a href="/category/list" className="list-group-item list-group-item-action">Category List</a>
+            <a href="/category/create" className="list-group-item list-group-item-action sidebar-dashboard">Create Category</a>
+            <a href="/category/list" className="list-group-item list-group-item-action sidebar-dashboard">Category List</a>
             {/* Add more links for category-related actions here */}
           </div>
 
-          <button onClick={toggleProduct} className="list-group-item list-group-item-action" data-toggle="collapse" data-target="#productDropdown">
+          <button onClick={toggleProduct} className="list-group-item list-group-item-action
+          sidebar-dashboard" data-toggle="collapse" data-target="#productDropdown">
             PRODUCTS
           </button>
           <div id="productDropdown" className={`collapse ${isProductOpen ? 'show' : ''}`}>
-            <a href="/product/create" className="list-group-item list-group-item-action">Create Product</a>
-            <a href="/product/list" className="list-group-item list-group-item-action">Product List</a>
+            <a href="/product/create" className="list-group-item list-group-item-action sidebar-dashboard">Create Product</a>
+            <a href="/product/list" className="list-group-item list-group-item-action sidebar-dashboard">Product List</a>
           </div>
           
         </div>

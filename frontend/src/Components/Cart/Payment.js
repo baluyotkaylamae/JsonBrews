185ed587c8e726652e73e1a137c1b1bb31849311
baluyotkaylamae <kaylamae.baluyot@tup.edu.ph>
2023-11-11 17:43:06 +0800

    modify shipping

--- frontend/src/Components/Cart/Payment.js
+++ frontend/src/Components/Cart/Payment.js
@@ -1,116 +1,69 @@
-import React, { Fragment, useEffect, useState } from 'react'
-import { useNavigate } from "react-router-dom";
-import MetaData from '../Layouts/Metadata'
-import CheckoutSteps from './CheckoutSteps'
-import axios from 'axios';
+import React, { Fragment, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import MetaData from '../Layouts/Metadata';
+import CheckoutSteps from './CheckoutSteps';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { getToken } from '../../utils/helpers';
 
+const Payment = ({ cartItems, shippingInfo }) => {
+  const [loading, setLoading] = useState(true);
+  let navigate = useNavigate();
+  const order = {
+    orderItems: cartItems,
+    shippingInfo,
+  };
 
-const Payment = ({cartItems, shippingInfo}) => {
-    const [loading, setLoading] = useState(true)
-    let navigate = useNavigate();
-    const order = {
-        orderItems: cartItems,
-        shippingInfo
+  const orderInfo = JSON.parse(sessionStorage.getItem('orderInfo'));
+  if (orderInfo) {
+    order.itemsPrice = orderInfo.itemsPrice;
+    order.shippingPrice = orderInfo.shippingPrice;
+    order.taxPrice = orderInfo.taxPrice;
+    order.totalPrice = orderInfo.totalPrice;
+  }
+
+  const createOrder = async (order) => {
+    try {
+
+      toast.success('Order created', {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+      navigate('/success');
+    } catch (error) {
+      toast.error(error.response?.data?.message || 'Failed to create order', {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
     }
+  };
 
-    const orderInfo = JSON.parse(sessionStorage.getItem('orderInfo'));
-    if (orderInfo) {
-        order.itemsPrice = orderInfo.itemsPrice
-        order.shippingPrice = orderInfo.shippingPrice
-        order.taxPrice = orderInfo.taxPrice
-        order.totalPrice = orderInfo.totalPrice
-    }
-
-    const createOrder = async (order) => {
-        try {
-            const config = {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${getToken()}`
-                }
-            }
-            const { data } = await axios.post(`http://localhost:4001/api/order/new`, order, config)
-            // setIsUpdated(data.success)
-            setLoading(false)
-            toast.success('order created', {
-                position: toast.POSITION.BOTTOM_RIGHT
-            });
-           
-            
-            navigate('/success')
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    document.querySelector('#pay_btn').disabled = true;
     
-        } catch (error) {
-            toast.error(error.response.data.message, {
-                position: toast.POSITION.BOTTOM_RIGHT
-            });
-           }
-    }
+    createOrder(order);
+  };
 
-    const submitHandler = async (e) => {
-        e.preventDefault();
-        document.querySelector('#pay_btn').disabled = true;
-        order.paymentInfo = {
-            id: 'pi_1DpdYh2eZvKYlo2CYIynhU32',
-            status: 'succeeded'
-        }
-        createOrder(order)
-        navigate('/success')
-      }
+  return (
+    <Fragment>
+      <MetaData title={'Payment'} />
+      <CheckoutSteps shipping confirmOrder payment />
+      <div className="row wrapper">
+        <div className="col-10 col-lg-5">
+          <form className="shadow-lg" onSubmit={submitHandler}>
+            <h1 className="mb-4">Payment Method</h1>
+            <p>Cash on Delivery (COD)</p>
+            <button
+              id="pay_btn"
+              type="submit"
+              className="btn btn-block py-3"
+            >
+              Place Order - {`₱${orderInfo && orderInfo.totalPrice}`}
+            </button>
+          </form>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 
-    return (
-        <Fragment>
-            <MetaData title={'Payment'} />
-            <CheckoutSteps shipping confirmOrder payment />
-            <div className="row wrapper">
-                <div className="col-10 col-lg-5">
-                    <form className="shadow-lg" onSubmit={submitHandler}>
-                        <h1 className="mb-4">Card Info</h1>
-                        <div className="form-group">
-                            <label htmlFor="card_num_field">Card Number</label>
-                            <input
-                                type="text"
-                                id="card_num_field"
-                                className="form-control"
-                                
-                            />
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="card_exp_field">Card Expiry</label>
-                            <input
-                                type="text"
-                                id="card_exp_field"
-                                className="form-control"
-                               
-                            />
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="card_cvc_field">Card CVC</label>
-                            <input
-                                type="text"
-                                id="card_cvc_field"
-                                className="form-control"
-                                
-                            />
-                        </div>
-                        <button
-                            id="pay_btn"
-                            type="submit"
-                            className="btn btn-block py-3"
-                        >
-                            Pay {` - ${orderInfo && orderInfo.totalPrice}`}
-                        </button>
-
-                    </form>
-                </div>
-            </div>
-
-        </Fragment>
-    )
-}
-
-export default Payment
+export default Payment;

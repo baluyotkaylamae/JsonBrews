2f7005ab30b318f6c8a9f6f43b3df4ff74ec547a
armangracia <graciaarmansteven@gmail.com>
2023-11-25 10:16:28 +0800

    fetching state for user details

--- frontend/src/Components/User/UpdateProfile.js
+++ frontend/src/Components/User/UpdateProfile.js
@@ -1,75 +1,70 @@
-import React, { Fragment, useState, useEffect } from 'react'
-import MetaData from '../Layouts/Metadata'
-import { useNavigate } from "react-router-dom";
+import React, { Fragment, useState, useEffect } from 'react';
+import MetaData from '../Layouts/Metadata';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { getToken } from '../../utils/helpers';
 
 const UpdateProfile = () => {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [avatar, setAvatar] = useState('')
-    const [avatarPreview, setAvatarPreview] = useState('/images/default_avatar.jpg')
-    const [error, setError] = useState('')
-    const [user, setUser] = useState({})
-    const [loading, setLoading] = useState(false)
-    const [isUpdated, setIsUpdated] = useState(false)
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [avatar, setAvatar] = useState('');
+    const [avatarPreview, setAvatarPreview] = useState('/images/default_avatar.jpg');
+    const [error, setError] = useState('');
+    const [user, setUser] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [isUpdated, setIsUpdated] = useState(false);
     let navigate = useNavigate();
 
     const getProfile = async () => {
         const config = {
             headers: {
-                // 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${getToken()}`
             }
-        }
+        };
+
         try {
-            const { data } = await axios.get(`http://localhost:4001/api/v1/me`, config)
-            console.log(data)
-            // setUser(data.user)
+            const { data } = await axios.get(`http://localhost:4001/api/me`, config);
             setName(data.user.name);
             setEmail(data.user.email);
-            setAvatarPreview(data.user.avatar.url)
-            setLoading(false)
+            setAvatarPreview(data.user.avatar.url);
+            setLoading(false);
         } catch (error) {
-            toast.error('user not found', {
-                position: toast.POSITION.BOTTOM_RIGHT
+            console.error('Error fetching profile:', error);
+            toast.error('Error fetching user profile', {
+                position: toast.POSITION.BOTTOM_RIGHT,
             });
         }
-    }
+    };
 
     const updateProfile = async (userData) => {
         const config = {
             headers: {
                 'Content-Type': 'multipart/form-data',
-                'Authorization': `Bearer ${getToken()}`
-            }
-        }
+                'Authorization': `Bearer ${getToken()}`,
+            },
+        };
+
         try {
-            const { data } = await axios.put(`${process.env.REACT_APP_API}/api/me/update`, userData, config)
-            setIsUpdated(data.success)
-            setLoading(false)
-            toast.success('user updated', {
-                position: toast.POSITION.BOTTOM_RIGHT
+            const { data } = await axios.put(`${process.env.REACT_APP_API}/api/me/update`, userData, config);
+            setIsUpdated(data.success);
+            setLoading(false);
+            toast.success('User updated', {
+                position: toast.POSITION.BOTTOM_RIGHT,
             });
-            //  getProfile();
-            navigate('/me', { replace: true })
-
-
+            navigate('/me', { replace: true });
         } catch (error) {
-            console.log(error)
-            toast.error('user not found', {
-                position: toast.POSITION.BOTTOM_RIGHT
+            console.log(error);
+            toast.error('User not found', {
+                position: toast.POSITION.BOTTOM_RIGHT,
             });
         }
-    }
+    };
 
-    // console.log(error)
     useEffect(() => {
-        getProfile()
-
-    }, [])
+        getProfile();
+    }, []); // Empty dependency array ensures the effect runs only once when the component mounts
 
     const submitHandler = (e) => {
         e.preventDefault();
@@ -77,30 +72,31 @@ const UpdateProfile = () => {
         formData.set('name', name);
         formData.set('email', email);
         formData.set('avatar', avatar);
-        updateProfile(formData)
-    }
+        updateProfile(formData);
+    };
 
-    const onChange = e => {
+    const onChange = (e) => {
         const reader = new FileReader();
 
         reader.onload = () => {
             if (reader.readyState === 2) {
-                setAvatarPreview(reader.result)
-                setAvatar(reader.result)
+                setAvatarPreview(reader.result);
+                setAvatar(reader.result);
             }
-        }
+        };
 
-        reader.readAsDataURL(e.target.files[0])
+        reader.readAsDataURL(e.target.files[0]);
+    };
 
-    }
-    console.log(user)
+    console.log(user);
+
     return (
         <Fragment>
             <MetaData title={'Update Profile'} />
 
             <div className="row update-profile-bg">
                 <div className="col-10 col-lg-5">
-                    <form className="shadow-lg" onSubmit={submitHandler} encType='multipart/form-data'>
+                    <form className="shadow-lg" onSubmit={submitHandler} encType="multipart/form-data">
                         <h1 className="mt-2 mb-5">Update Profile</h1>
 
                         <div className="form-group">
@@ -109,7 +105,7 @@ const UpdateProfile = () => {
                                 type="name"
                                 id="name_field"
                                 className="form-control"
-                                name='name'
+                                name="name"
                                 value={name}
                                 onChange={(e) => setName(e.target.value)}
                             />
@@ -121,46 +117,52 @@ const UpdateProfile = () => {
                                 type="email"
                                 id="email_field"
                                 className="form-control"
-                                name='email'
+                                name="email"
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
                             />
                         </div>
 
-                        <div className='form-group'>
-                            <label htmlFor='avatar_upload'>Avatar</label>
-                            <div className='d-flex align-items-center'>
+                        <div className="form-group">
+                            <label htmlFor="avatar_upload">Avatar</label>
+                            <div className="d-flex align-items-center">
                                 <div>
-                                    <figure className='avatar mr-3 item-rtl'>
+                                    <figure className="avatar mr-3 item-rtl">
                                         <img
                                             src={avatarPreview}
-                                            className='rounded-circle'
-                                            alt='Avatar Preview'
+                                            className="rounded-circle"
+                                            alt="Avatar Preview"
                                         />
                                     </figure>
                                 </div>
-                                <div className='custom-file'>
+                                <div className="custom-file">
                                     <input
-                                        type='file'
-                                        name='avatar'
-                                        className='custom-file-input'
-                                        id='customFile'
-                                        accept='image/*'
+                                        type="file"
+                                        name="avatar"
+                                        className="custom-file-input"
+                                        id="customFile"
+                                        accept="image/*"
                                         onChange={onChange}
                                     />
-                                    <label className='custom-file-label' htmlFor='customFile'>
+                                    <label className="custom-file-label" htmlFor="customFile">
                                         Choose Avatar
                                     </label>
                                 </div>
                             </div>
                         </div>
 
-                        <button type="submit" className="btn update-btn btn-block mt-4 mb-3" disabled={loading ? true : false} >Update</button>
+                        <button
+                            type="submit"
+                            className="btn update-btn btn-block mt-4 mb-3"
+                            disabled={loading ? true : false}
+                        >
+                            Update
+                        </button>
                     </form>
                 </div>
             </div>
         </Fragment>
-    )
-}
+    );
+};
 
-export default UpdateProfile
+export default UpdateProfile;
